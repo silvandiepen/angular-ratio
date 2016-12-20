525da7c8ad70762fe9d3d8543c610801c4aefe04
silvandiepen <me@silvandiepen.com>
2016-12-20 14:08:45 +0100

    shorten code

--- angular-ratio.js
+++ angular-ratio.js
@@ -22,30 +22,20 @@
 
       var setRatio = function setRatio(container){
         // get ratio elements
-          var elements = container.querySelectorAll('[class*="ratio"]');
-          for (var i = 0; i < elements.length; i++) {
+        var elements = container.querySelectorAll('[class*="ratio"]');
+        for (var i = 0; i < elements.length; i++) {
 
-            var classNames = elements[i].className;
-	    			var sizeElement = elements[i];
-	    			var classArray = classNames.split(' ');
+          var classNames = elements[i].className;
+    			var sizeElement = elements[i];
+    			var classArray = classNames.split(' ');
 
-	    		  for (var a = 0; a < classArray.length; a++){
-	    			// split values
-	    				var className = classArray[a].split("-");
+    		  for (var a = 0; a < classArray.length; a++){
+    			// split values
+    				var className = classArray[a].split("-");
 
-	    				if(className[0] == "ratio") {
-
-	    				var rh = className[2];
-	    				var rw = className[1];
-
-	    			// return width
-	    				var w = sizeElement.offsetWidth;
-
-	    			// return ratio height
-	            var height = (w / (rh/rw));
-
-	    			// set height
-	    				sizeElement.style.height = height +"px";
+    				if(className[0] == "ratio") {
+              // set height
+	    				sizeElement.style.height = (sizeElement.offsetWidth / className[2]/className[1]) +"px";
 	    			}
           }
         }
